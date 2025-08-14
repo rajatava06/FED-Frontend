9ef946abba2f3fc6fd13482007e5936dbbf5e526
Rajatava Das <rajatava2006@gmail.com>
2025-08-14 22:15:05 +0530

    omega5

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -568,21 +568,7 @@ const EventCard = (props) => {
             </div>
           )}
           
-          {/* See Team Details Button - Only show for team events when user is registered
-          {type === "ongoing" && 
-           info.participationType === "Team" && 
-           btnTxt === "Already Registered" && 
-           authCtx.isLoggedIn && (
-            <div style={{ marginTop: "10px", textAlign: "center" }}>
-              <button
-                className={style.teamDetailsBtn}
-                onClick={() => setIsTeamDetailsOpen(true)}
-              >
-                <FaEye size={14} />
-               
-              </button>
-            </div>
-          )} */}
+
         </div>
         <div className={style.backtxt} style={customStyles.backtxt}>
           <div style={{ display: "flex", alignItems: "center" }}>
